1785e2d02998efb5320dae7b6908ec82b05b152e
Levi27610 <levi22m@gmail.com>
2021-07-11 16:09:19 +0300

    this is my first exercise

--- LESSON1/110721.py
+++ LESSON1/110721.py
@@ -1,3 +1,18 @@
-print("hello world")
-for i in range(10):
-    print(i)
+num=int(input("Enter a number with 4 digits:"))
+'''
+    Alafim=4
+    Meot=5
+    Asarot=6
+    Ahadot=7
+ '''
+#this is Alafim
+print("Alafim= " + str (num//1000))
+#this is Meot
+num=(num%1000)
+print("Meot= " + str (num//100))
+#this is Asarot
+num=(num%100)
+print("Asarot= " + str (num//10))
+#this is Ahadot
+num=(num%10)
+print("Ahadot= " + str (num))
